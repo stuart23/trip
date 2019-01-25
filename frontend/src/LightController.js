6483360f56cbed363c87edead4f814fae4ef4cb3
Stuart Buckingham <stuart@localhost.localdomain>
2019-01-25 14:46:21 -0800

    Styling works for buttons

--- frontend/src/LightController.js
+++ frontend/src/LightController.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import './LightController.css';
+import { withStyles } from '@material-ui/styles';
 
-export default class LightController extends Component {
+const styles = {
+  on_button: {
+    background: 'linear-gradient(45deg, #F0FF6B 30%, #7CFF6B 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'black',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  off_button: {
+    background: 'linear-gradient(45deg, #C15169 30%, #BC693E 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+};
+
+class LightController extends Component {
   constructor(props) {
     super(props);
     this.switchOn = this.switchOn.bind(this);
@@ -36,15 +57,18 @@ export default class LightController extends Component {
   }
 
   render() {
+    const {classes} = this.props;
     return(
       <div>
-        <Button variant="contained" color="111111" onClick={this.switchOn}>
+        <Button className={classes.on_button} onClick={this.switchOn}>
           Switch On
         </Button>
-        <Button variant="contained" className="DarkButton" onClick={this.switchOff}>
+        <Button className={classes.off_button} onClick={this.switchOff}>
           Switch Off
         </Button>
       </div>
     )
   }  
 }
+
+export default withStyles(styles)(LightController);
